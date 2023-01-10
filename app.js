141b1c3abe6c4098ce7444dd6a4174ff2f421f42
zeindelic@gmail.com <zeindelic@gmail.com>
2023-01-10 09:05:27 -0800

    commit

---
+++ app.js
@@ -0,0 +1,99 @@
+// let arr = [3, 4, 5, 6];
+
+// for (let i = 0; i < arr.length; i++){
+//   arr[i] = arr[i] * 3;
+// }
+
+// console.log(arr); // [9, 12, 15, 18]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let arr = [3, 4, 5, 6];
+
+// const newArr = arr.map(myFunction)
+
+// function myFunction(num) {
+//   return num * 10;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// empty = []
+// let modifiedArr = arr.map(function(num) {
+//     if (num % 2 == 0) {
+//         empty.push(num);
+//     }
+// })
+// console.log(empty);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let modifiedArr = arr.map(function(el, index){
+// return index, el*el
+// })
+  
+// console.log(modifiedArr)
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Make an array of numbers that are doubles of the first array
+
+// Take an array of numbers and make them strings
+
+//  Capitalize each of an array of names
+
